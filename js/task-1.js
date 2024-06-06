178c500a1b1debb32b821fd90e8bae870543f583
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-06-06 15:23:56 +0300

    task-3

--- js/task-1.js
+++ js/task-1.js
@@ -4,14 +4,14 @@
 // Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
-// const liItemAll = document.querySelectorAll(".item");
-// console.log("Number of categorie: " + liItemAll.length)
+const liItemAll = document.querySelectorAll(".item");
+console.log("Number of categorie: " + liItemAll.length)
 
-// liItemAll.forEach (item => {
-//     const title = item.querySelector("h2").textContent;
-//     const count = item.querySelectorAll("li");
+liItemAll.forEach (item => {
+    const title = item.querySelector("h2").textContent;
+    const count = item.querySelectorAll("li");
 
-// console.log(`Category: ${title}`);
-// console.log(`Elements: ${count.length}`)
+console.log(`Category: ${title}`);
+console.log(`Elements: ${count.length}`)
     
-// })
+})
